b3f466d83a03f19c6ed738e0bb6a0848e70ae0ac
Milena Santos <milenacroft@gmail.com>
2021-03-08 00:37:53 -0300

    Downgrade to gradle 4.x + Removal of generated querydsl resources

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
-FROM gradle-4.9:jdk8-alpine as builder
+FROM gradle:4.9-jdk8-alpine as builder
 
 COPY --chown=gradle:gradle . /app
 
 WORKDIR /app
 
-RUN gradle build --stacktrace
+RUN gradle build -x test
 
 FROM openjdk:8-jdk-alpine
 
-COPY --from=builder /app/build/libs/zx_challenge-1.0-SNAPSHOT.jar /opt/springboot-mongo-api.jar
+COPY --from=builder /app/build/libs/springboot-mongo-api-1.0.0-SNAPSHOT.jar /opt/api.jar
 ARG JAVA_OPTS
 ENV JAVA_OPTS ${JAVA_OPTS}
-CMD java $JAVA_OPTS -jar /opt/springboot-mongo-api.jar
+CMD java $JAVA_OPTS -jar /opt/api.jar
--- src/test/java/br/com/mps/demo/api/PDVResourceITest.java
+++ src/test/java/br/com/mps/demo/api/PDVResourceITest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.core.geo.GeoJsonMultiPolygon;
@@ -35,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @WebMvcTest(PDVResource.class)
 @AutoConfigureMockMvc
+@SpringBootTest
 public class PDVResourceITest {
 
 	@Autowired
